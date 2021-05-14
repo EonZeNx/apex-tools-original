ee0ef43ac149fedc441e88bd082e743f2802f8c5
EonZeNx <sparck.va@gmail.com>
2021-05-14 16:20:39 +1200

    Added feature: Rehash - New functions allow you to run a Jenkins Lookup3, the hashing algorithm for Just Cause 3 (And maybe other Avalanche projects) - On loading RTPC and IRTPC XMLs, will now rehash the name attribute.

--- EonZeNx.ApexTools.Core/Utils/XmlUtils.cs
+++ EonZeNx.ApexTools.Core/Utils/XmlUtils.cs
@@ -1,17 +1,24 @@
-﻿using System.Xml;
+﻿using System.Text;
+using System.Xml;
 using EonZeNx.ApexTools.Configuration;
 
 namespace EonZeNx.ApexTools.Core.Utils
 {
     public static class XmlUtils
     {
+        /// <summary>
+        /// Attempts to get an attribute from an XML reader
+        /// </summary>
+        /// <param name="xr">XML reader</param>
+        /// <param name="attribute">Attribute name</param>
+        /// <returns>String if found, null if not</returns>
+        /// <exception cref="XmlException">Throws exception if the element has no attributes</exception>
         public static string GetAttribute(XmlReader xr, string attribute)
         {
             if (!xr.HasAttributes) throw new XmlException("Missing attributes");
             
             var attributeValue = xr.GetAttribute(attribute);
-            if (attributeValue == null) throw new XmlException($"Missing attribute '{attribute}'");
-            
+
             return attributeValue;
         }
 
@@ -25,6 +32,17 @@ namespace EonZeNx.ApexTools.Core.Utils
             {
                 xw.WriteAttributeString("Name", name);
             }
+        }
+
+        public static int ReadNameIfValid(XmlReader xr)
+        {
+            var name = GetAttribute(xr, "Name");
+            if (name == null)
+            {
+                return ByteUtils.HexToInt(GetAttribute(xr, "NameHash"));
+            }
+            
+            return HashUtils.HashJenkinsL3(Encoding.UTF8.GetBytes(name));
         }
     }
 }
