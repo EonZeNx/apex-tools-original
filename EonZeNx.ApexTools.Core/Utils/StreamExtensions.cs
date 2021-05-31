eb199c40745c0b9a4e039261d2e9d01332d502a8
EonZeNx <sparck.va@gmail.com>
2021-05-31 18:40:28 +1200

    Read stream extensions

--- EonZeNx.ApexTools.Core/Utils/StreamExtensions.cs
+++ EonZeNx.ApexTools.Core/Utils/StreamExtensions.cs
@@ -8,7 +8,7 @@ namespace EonZeNx.ApexTools.Core.Utils
     /// </summary>
     public static class StreamExtensions
     {
-        #region Mantissa
+        #region Write Mantissa
 
         public static void Write(this Stream s, double value)
         {
@@ -22,7 +22,7 @@ namespace EonZeNx.ApexTools.Core.Utils
 
         #endregion
 
-        #region Signed
+        #region Write Signed
 
         public static void Write(this Stream s, long value)
         {
@@ -46,7 +46,7 @@ namespace EonZeNx.ApexTools.Core.Utils
 
         #endregion
 
-        #region Unsigned
+        #region Write Unsigned
 
         public static void Write(this Stream s, ulong value)
         {
@@ -66,6 +66,106 @@ namespace EonZeNx.ApexTools.Core.Utils
         public static void Write(this Stream s, byte value)
         {
             s.Write(BitConverter.GetBytes(value));
+        }
+
+        #endregion
+
+
+        #region Read Mantissa
+
+        public static double ReadDouble(this Stream s)
+        {
+            var value = new byte[8];
+            s.Read(value, 0, 8);
+
+            return BitConverter.ToDouble(value);
+        }
+        
+        public static float ReadSingle(this Stream s)
+        {
+            var value = new byte[4];
+            s.Read(value, 0, 4);
+
+            return BitConverter.ToSingle(value);
+        }
+
+        #endregion
+
+        #region Read Signed
+
+        public static long ReadInt64(this Stream s)
+        {
+            var value = new byte[8];
+            s.Read(value, 0, 8);
+
+            return BitConverter.ToInt64(value);
+        }
+        
+        public static int ReadInt32(this Stream s)
+        {
+            var value = new byte[4];
+            s.Read(value, 0, 4);
+
+            return BitConverter.ToInt32(value);
+        }
+        
+        public static short ReadInt16(this Stream s)
+        {
+            var value = new byte[2];
+            s.Read(value, 0, 2);
+
+            return BitConverter.ToInt16(value);
+        }
+        
+        public static sbyte ReadSByte(this Stream s)
+        {
+            return (sbyte) s.ReadByte();
+        }
+
+        #endregion
+
+        #region Read Unsigned
+
+        public static ulong ReadUInt64(this Stream s)
+        {
+            var value = new byte[8];
+            s.Read(value, 0, 8);
+
+            return BitConverter.ToUInt64(value);
+        }
+        
+        public static uint ReadUInt32(this Stream s)
+        {
+            var value = new byte[4];
+            s.Read(value, 0, 4);
+
+            return BitConverter.ToUInt32(value);
+        }
+        
+        public static ushort ReadUInt16(this Stream s)
+        {
+            var value = new byte[2];
+            s.Read(value, 0, 2);
+
+            return BitConverter.ToUInt16(value);
+        }
+        
+        public static byte ReadUByte(this Stream s)
+        {
+            return (byte) s.ReadByte();
+        }
+
+        #endregion
+
+
+        #region Read Multi
+
+        public static byte[] ReadBytes(this Stream s, int count)
+        {
+            var value = new byte[count];
+            s.Read(value, 0, count);
+
+            return value;
         }
 
         #endregion
